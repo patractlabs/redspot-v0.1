2c7cb156f01715f99126f2807c5e64fe49d46c1d
opvqo <wxoxc@outlook.com>
2020-08-27 21:03:27 +1200

    feat: add template

---
+++ packages/redspot-template/templates/delegator/accumulator/lib.rs
@@ -0,0 +1,47 @@
+// Copyright 2019-2020 Parity Technologies (UK) Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#![cfg_attr(not(feature = "std"), no_std)]
+
+pub use self::accumulator::Accumulator;
+use ink_lang as ink;
+
+#[ink::contract(version = "0.1.0")]
+mod accumulator {
+    /// Holds a simple i32 value that can be incremented and decremented.
+    #[ink(storage)]
+    struct Accumulator {
+        value: i32,
+    }
+
+    impl Accumulator {
+        /// Initializes the value to the initial value.
+        #[ink(constructor)]
+        fn new(init_value: i32) -> Self {
+            Self { value: init_value }
+        }
+
+        /// Mutates the internal value.
+        #[ink(message)]
+        fn inc(&mut self, by: i32) {
+            self.value += by;
+        }
+
+        /// Returns the current state.
+        #[ink(message)]
+        fn get(&self) -> i32 {
+            self.value
+        }
+    }
+}
