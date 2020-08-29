34c9293c63add97ab758a0bd67772bd82ca23363
opvqo <wxoxc@outlook.com>
2020-08-29 12:43:58 +1200

    feat: add artifacts

--- packages/test-env/index.ts
+++ packages/test-env/index.ts
@@ -1,11 +1,14 @@
 import NodeEnvironment from 'jest-environment-node';
 import { RedspotConfig } from '@redspot/config';
+import { Resolver } from '@redspot/resolver';
 
 class RedSpotTestEnv extends NodeEnvironment {
   async setup() {
     await super.setup();
     //@ts-ignore
     this.context.config = new RedspotConfig(this.context?.network);
+    //@ts-ignore
+    this.context.artifacts = new Resolver(this.context.config);
   }
 
   async teardown() {
