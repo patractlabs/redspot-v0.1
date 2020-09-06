19be6914debc1a7b6a421c26300b843c87f8bc49
opvqo <wxoxc@outlook.com>
2020-09-07 00:29:36 +1200

    check cargo contract

--- packages/redspot/utils/checkRustEnv.ts
+++ packages/redspot/utils/checkRustEnv.ts
@@ -1,11 +1,27 @@
 import { execSync } from 'child_process';
 import chalk from 'chalk';
+import semver from 'semver';
 
 export function checkContractCli() {
+  let version: string;
   try {
-    execSync('cargo contract -V');
+    const versionData = execSync('cargo contract -V');
+    version = versionData.toString().split(' ')[1];
   } catch {
-    console.log(chalk.red('ERROR: `cargo-contract` not found'));
+    console.log(chalk.red('ERROR: No `cargo-contract` found'));
+    console.log(`Run the following command to install it:`);
+    console.log(
+      chalk.cyan(`$ cargo install --git https://github.com/paritytech/cargo-contract cargo-contract --force`),
+    );
+    process.exit(1);
+  }
+
+  if (semver.lt(version, '0.7.0')) {
+    console.log(chalk.red('ERROR: `cargo-contract` requires v0.7.0 or above'));
+    console.log(`Run the following command to install it:`);
+    console.log(
+      chalk.cyan(`$ cargo install --git https://github.com/paritytech/cargo-contract cargo-contract --force`),
+    );
     process.exit(1);
   }
 }
