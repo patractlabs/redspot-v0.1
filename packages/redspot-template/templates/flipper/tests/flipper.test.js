696133f888ad140ed7c7b0e6f5cb10c52cbd8782
opvqo <wxoxc@outlook.com>
2020-09-09 17:36:36 +1200

    fix: flipper test

--- packages/redspot-template/templates/flipper/tests/flipper.test.js
+++ packages/redspot-template/templates/flipper/tests/flipper.test.js
@@ -1,34 +1,31 @@
-const rsconfig = require("../redspot-config.js");
-const flipper = artifacts.require("flipper");
+const rsconfig = require('../redspot-config.js');
+const flipper = artifacts.require('flipper');
 
-describe("flipper test", () => {
-  const Alice = config.pairs[0];
+describe('flipper test', () => {
+  let Alice;
   let flipperApi;
 
   beforeAll(async () => {
-    flipperApi = await flipper.deployed(
-      Alice,
-      await flipper.putCode(Alice),
-      flipper.abi.constructors[0](false)
-    );
-  })
+    Alice = config.pairs[0];
+    flipperApi = await flipper.deployed(Alice, await flipper.putCode(Alice), flipper.abi.constructors[0](false));
+  });
 
-  test("account", () => {
-    expect("0x" + Buffer.from(Alice.publicKey).toString("hex")).toBe(
-      rsconfig.networks.development.accounts[0].publicKey
+  test('account', () => {
+    expect('0x' + Buffer.from(Alice.publicKey).toString('hex')).toBe(
+      rsconfig.networks.development.accounts[0].publicKey,
     );
-  })
+  });
 
-  test("flipper", async () => {
+  test('flipper', async () => {
     const beforeValue = await flipperApi.messages.get().call();
-    console.log('Current value is', beforeValue.toString())
-    console.log('Flip')
+    console.log('Current value is', beforeValue.toString());
+    console.log('Flip');
     const txResult = await flipperApi.messages.flip().send({
       from: Alice,
     });
-    expect(txResult.status).toEqual('success')
+    expect(txResult.status).toEqual('success');
     const afterValue = await flipperApi.messages.get().call();
-    console.log('Current value is', afterValue.toString())
-    expect(afterValue.toJSON()).toEqual(!beforeValue.toJSON())
-  })
-})
+    console.log('Current value is', afterValue.toString());
+    expect(afterValue.toJSON()).toEqual(!beforeValue.toJSON());
+  });
+});
